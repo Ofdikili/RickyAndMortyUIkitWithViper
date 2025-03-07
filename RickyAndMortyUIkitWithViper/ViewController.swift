f82a097b018c9bb6bb7c0ca7fdf3d7f8d95d2118
Ofdikili <omerfarukdikili00@gmail.com>
2025-03-08 01:59:46 +0300

    Initial Commit

---
+++ RickyAndMortyUIkitWithViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMortyUIkitWithViper
+//
+//  Created by Ömer Faruk Dikili on 8.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RickyAndMortyUIkitWithViperTests/RickyAndMortyUIkitWithViperTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyUIkitWithViperTests.swift
+//  RickyAndMortyUIkitWithViperTests
+//
+//  Created by Ömer Faruk Dikili on 8.03.2025.
+//
+
+import Testing
+@testable import RickyAndMortyUIkitWithViper
+
+struct RickyAndMortyUIkitWithViperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
